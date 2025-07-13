bc4fd3d936b241ad72940081c993fe65c4df9b51
29th-Day <43274844+29th-Day@users.noreply.github.com>
2025-07-13 02:06:48 +0200

    basics working

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js,ts}', './src/**/*.{html,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
